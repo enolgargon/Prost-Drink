5b5b9d594e8834f79c3f8e2e087a6877ee765808
Samuel <uo238524@uniovi.es>
2018-12-22 18:24:46 +0100

    Creado TrabalenguasActivity

---
+++ app/src/main/java/com/cgi/sdm_project/igu/juego/loop/TrabalenguasActivity.java
@@ -0,0 +1,15 @@
+package com.cgi.sdm_project.igu.juego.loop;
+
+import android.view.View;
+
+import com.cgi.sdm_project.logica.juego.activities.IFinJuego;
+import com.cgi.sdm_project.logica.juego.activities.InicioJuego;
+
+public class TrabalenguasActivity extends Loop implements InicioJuego, IFinJuego {
+
+
+    @Override
+    public void cargarSiguienteJuego(View view) {
+
+    }
+}
