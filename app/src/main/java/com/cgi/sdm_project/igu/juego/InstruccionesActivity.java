f8faf83d6fdd1940218e26e63d573fdc810d38ce
Enol García González <uo257775@uniovi.es>
2018-12-26 16:59:31 +0100

    Creado el dialogo de la ventana principal para las instrucciones. Vamos con la ventana antes de empezar el juego

---
+++ app/src/main/java/com/cgi/sdm_project/igu/juego/InstruccionesActivity.java
@@ -0,0 +1,15 @@
+package com.cgi.sdm_project.igu.juego;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cgi.sdm_project.R;
+
+public class InstruccionesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrucciones);
+    }
+}
